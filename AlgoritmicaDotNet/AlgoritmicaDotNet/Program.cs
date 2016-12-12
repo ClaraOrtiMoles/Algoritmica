5b269a3a76724e7459f77862c0299a0951eac759
Clara Orti <claraortimoles@gmail.com>
2016-12-12 23:50:28 +0100

    HanoiTower

--- AlgoritmicaDotNet/AlgoritmicaDotNet/Program.cs
+++ AlgoritmicaDotNet/AlgoritmicaDotNet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AlgoritmicaDotNet.DP;
+using AlgoritmicaDotNet.Recursive;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,112 @@ namespace AlgoritmicaDotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("MaxSubArrayOf: {3,1,-2,-3,-1,3,2,-2,-1}");
-            int[] array = new int[9] { 3, 1, -2, -3, -1, 3, 2, -2, -1 };
-            var maxSubArray = MaxSubArray.GetMaxSubArray(array);
-            Console.WriteLine(maxSubArray);
-            Console.ReadLine();
+            int userInput = 0;
+            do
+            { 
+                userInput = DisplayMenu();
+                ManageOption(userInput);
+              
+            } while (userInput != 5);
+
+            
+        }
+
+
+        private static void ManageOption(int userInput)
+        {
+
+            switch (userInput)
+            {
+                case 1:
+                    CallSubArrayMethod();
+                    break;
+                case 2:
+                    CallHanoiTowerMethod();
+                    break;
+                case 5:
+                    break;
+            }
+             
+        }
+
+        private static void CallHanoiTowerMethod()
+        {
+            bool exit = false;
+            Console.WriteLine("Give me a number of discs or 's' to exit");
+            do
+            {
+                try
+                {
+                    var result = Console.ReadLine();
+                    if (result.Equals("s", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        exit = true;
+                    }
+                    else
+                    {
+                        var n = int.Parse(result);
+                        var moves = HanoiTower.GetResult(n);
+                        Console.WriteLine(moves);
+                        exit = true;
+                    }
+                   
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Give me a valid number or type 's' to come back to the menu");
+
+                }
+            } while (!exit);
+        }
+
+        private static void CallSubArrayMethod()
+        {
+            bool exit = false;
+            //Console.WriteLine("MaxSubArrayOf: {3,1,-2,-3,-1,3,2,-2,-1}");
+            Console.WriteLine("Give me an array, example: {3,2,-1,-1,-4,5,6,-2}");
+            do
+            {
+                try
+                {
+                    var result = Console.ReadLine();
+                    if (result.Equals("s", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        exit = true;
+                        
+                    }
+                    else
+                    {
+                        var consoleArray = result.Substring(1, result.Length - 2).Split(',');
+                        //int[] array = new int[9] { 3, 1, -2, -3, -1, 3, 2, -2, -1 };
+                        int[] array = consoleArray.Select(x => int.Parse(x)).ToArray();
+                        var maxSubArray = MaxSubArray.GetMaxSubArray(array);
+                        Console.WriteLine(maxSubArray);
+                        exit = true;
+                    }
+                    
+                    
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Give me the array in a correct form or type 's' to come back to the menu");
+
+                }
+            } while (!exit);
+
+            
+        }
+
+        public static int DisplayMenu()
+        {
+            Console.WriteLine("Programs");
+            Console.WriteLine();
+            Console.WriteLine("1. MaximInSubArray");
+            Console.WriteLine("2. Hanoi Tower"); 
+            Console.WriteLine("5. Exit");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+
         }
     }
 }
---
+++ AlgoritmicaDotNet/AlgoritmicaDotNet/Recursive/HanoiTower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmicaDotNet.Recursive
+{
+    public static class HanoiTower
+    {
+        private static string result;
+        public static string GetResult(int n)
+        {
+            result = string.Empty;
+            Move(n, "IZQUIERDA", "CENTRO", "DERECHA");
+            return result;
+        }
+
+        public static void Move(int n, string origin, string aux, string destine)
+        {
+            if (n == 0)
+            {
+                result = "There are 0 discs";
+            }
+            if (n == 1)
+            {
+                result = $"{result} Move: {n}, {origin}, {destine} \n"; 
+            }
+            else
+            {
+                Move(n - 1, origin, destine, aux);
+                result = $"{result} Move: 1, {origin}, {destine}";
+                Move(n - 1, aux, origin, destine);
+            }
+        }
+    }
+}
